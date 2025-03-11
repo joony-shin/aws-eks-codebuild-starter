c00dd99671e173527ade325c913747918e314469
joony-shin <89501763+joony-shin@users.noreply.github.com>
2025-03-11 19:54:18 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from AWS EKS with CondeBuild and ECR!"
+    return "Hello from AWS EKS with CondeBuild and ECR!!!!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
